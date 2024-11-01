d38325b1040a0b5c2089af233f0d052300d19564
Anjoelo Calderon <73631126+jelo-ca@users.noreply.github.com>
2024-10-31 21:21:41 -0700

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,32 @@
+import os
+from dotenv import load_dotenv
+import discord
+from discord.ext import commands
+import json
+
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = commands.Bot(command_prefix='&', intents=intents)
+
+@client.command()
+async def test(ctx):
+    print(f"passed")
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+        
+    await client.process_commands(message)
+client.run(TOKEN)
